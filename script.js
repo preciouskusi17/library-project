b9a7d1381789ada99c6c0cda3316a472eefe32cc
Precious Kusi <146889458+preciouskusi17@users.noreply.github.com>
2024-03-25 09:07:19 -0400

    Add some styles to the library page

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function addBookToLibrary(title,author,pages,read){
     showBox()
 }
 
-addBookToLibrary('precious', 'ama', 78, 'True')
-addBookToLibrary(' hilda', 'yaa', 478, '')
-addBookToLibrary(' yvone', 'ama', 878, 'true')
-addBookToLibrary('godwon','boy',98, '')
+addBookToLibrary('Jesus Always', 'Sarah Young', 400, '')
+addBookToLibrary(' Woman Evolve', ' Sarah Jakes Roberts', 224, '')
+addBookToLibrary('Believe for It', ' CeCe Winans', 256, 'true')
+addBookToLibrary('Throne Room',' Cece Winans',144, '')
     
 
 
@@ -39,11 +39,11 @@ function showBox(){
    
     let bookItem= document.createElement('div');
      bookItem.classList.add('mybooks');
-    bookItem.innerHTML=`<p>Book Title: ${book.title}</p> <p>Book Author: ${book.author}</p>
-         <p>Number of Pages: ${book.pages}</p> <p>Read Status: ${book.read?"Read": "Not Read Yet"}</p> 
+    bookItem.innerHTML=`<h2>Book Title: ${book.title}</h2> <p>Book Author: ${book.author}</p>
+         <p>Number of Pages: ${book.pages}</p> <p>Book Status: ${book.read?"Read": "Not Read Yet"}</p> 
             <div id="btn">
         <button class="remove" onclick="removeBook(${i})">Remove</button>
-        <button onclick= toggleRead(${i})>Change Read Status</button>
+        <button class="change" onclick= toggleRead(${i})>Change Status</button>
     </div> `;
     
     container.appendChild(bookItem)  
@@ -80,3 +80,4 @@ document.querySelector('#cancel').addEventListener('click',()=>{
     dialog.close();
     
 })
+
